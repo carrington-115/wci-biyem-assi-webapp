9adbeaa46f7a8da0b1b6d334fba26e87ca916e7b
Mark Carrington <frumarkcarrington@gmail.com>
2022-08-30 13:37:49 +0100

    created the pages folder for the home, about us, contact us, store, and the announcement pages, and added them to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Aboutus from "./components/pages/Aboutus";
+import Contactus from "./components/pages/Contact";
+import Store from "./components/pages/Store";
+import Announcements from "./components/pages/Announcements";
 import "./App.css";
 
 function App() {
-  return <div className="App">WINNERS' CHAPEL BIYEM ASSI SITE</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route element={<Home />} path="home" index />
+          <Route element={<Aboutus />} path="about-us" />
+          <Route element={<Announcements />} path="announcements" />
+          <Route element={<Contactus />} path="contact-us" />
+          <Route element={<Store />} path="store" />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
