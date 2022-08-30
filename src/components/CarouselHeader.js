b18c3f8d003e75cee82be38a6b264854549eeeb3
Mark Carrington <frumarkcarrington@gmail.com>
2022-08-30 14:51:49 +0100

    created the carousel header file

---
+++ src/components/CarouselHeader.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+function CarouselHeader() {
+  return (
+    <div>
+      <HeaderComponent />
+    </div>
+  );
+}
+
+function HeaderComponent() {
+  return <div>the header</div>;
+}
+
+export default CarouselHeader;
