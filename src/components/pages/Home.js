97b7049a8bf736d40c7fdbdc8c699674e2223f9d
Mark Carrington <frumarkcarrington@gmail.com>
2022-08-31 15:23:51 +0100

    added the reactivity of header on scrolling and embedded a youtube video

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 import CarouselHeader from "../CarouselHeader";
 
 function Home() {
   return (
-    <div>
+    <Container>
       <CarouselHeader />
+      <ContentBody>
+        <HomeMainBody />
+      </ContentBody>
+    </Container>
+  );
+}
+
+function HomeMainBody() {
+  return (
+    <div>
+      Main Body
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/xNRJwmlRBNU"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
   );
 }
 
 export default Home;
+
+const Container = styled.div``;
+const ContentBody = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
