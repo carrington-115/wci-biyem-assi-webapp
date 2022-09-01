630faab514c870a93df46bfa938f10c9de0d4ddf
Mark Carrington <frumarkcarrington@gmail.com>
2022-09-01 15:11:25 +0100

    added the about us section of the home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -11,7 +11,8 @@ function Home() {
       <CarouselHeader />
       <ContentBody>
         <div className="content-div">
-          <HomeMainBody />
+          <InvitationContent />
+          <AboutSection />
         </div>
         <div className="aside-div">
           <Aside />
@@ -21,9 +22,16 @@ function Home() {
   );
 }
 
-function HomeMainBody() {
+function InvitationContent() {
   return (
     <MainContainerContent>
+      {/* <div className="the-mandate">
+        <h1>
+          Now the Hour has Come to Liberate the World <br />
+          from all oppression of the devil <br />
+          through the preaching of the word of faith
+        </h1>
+      </div> */}
       <Fade bottom>
         <div className="invitation-container">
           <h1>
@@ -55,6 +63,7 @@ function HomeMainBody() {
               </h2>
             </h2>
           </h2>
+          <AllPrograms to="programs">All Our Programs</AllPrograms>
         </div>
       </Fade>
       <div className="video-content-update">
@@ -75,6 +84,16 @@ function HomeMainBody() {
     </MainContainerContent>
   );
 }
+function AboutSection() {
+  return (
+    <AboutContainer>
+      <h1>Want to know about us?</h1>
+      <div className="bishop-abt-div"></div>
+      <div className="wci-abt-div"></div>
+      <div className="mandate-div"></div>
+    </AboutContainer>
+  );
+}
 
 export default Home;
 
@@ -82,27 +101,39 @@ const Container = styled.div``;
 const ContentBody = styled.div`
   display: flex;
   justify-content: center;
-  padding-top: 30px;
   width: 100%;
   .content-div {
-    width: 90%;
+    width: 80%;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    overflow: hidden;
   }
   iframe {
     padding: 0;
     margin: 0;
   }
   .aside-div {
-    width: calc(100%-90%);
+    width: calc(100%-80%);
   }
 `;
 
 const MainContainerContent = styled.div`
+  padding-top: 30px;
+  background-color: rgba(211, 176, 150, 0.3);
+  width: 32cm;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   .invitation-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     background-color: white;
     width: 20cm;
-    height: 12cm;
+    height: 15cm;
     border-radius: 10px;
     box-shadow: 2px 5px 5px rgba(0, 0, 0, 0.4);
     h1 {
@@ -113,6 +144,7 @@ const MainContainerContent = styled.div`
       background-color: rgb(240, 195, 60);
       border-top-left-radius: 10px;
       border-top-right-radius: 10px;
+      width: 19.5cm;
     }
     h2 {
       font-family: times new roman;
@@ -153,7 +185,7 @@ const MainContainerContent = styled.div`
     justify-content: center;
     align-items: center;
     margin-top: 20px;
-    margin-bottom: 20px;
+    margin-bottom: 2.5cm;
     iframe {
       box-shadow: 2px 5px 5px rgba(0, 0, 0, 0.4);
       border-bottom-left-radius: 10px;
@@ -169,10 +201,52 @@ const MainContainerContent = styled.div`
       border-top-right-radius: 10px;
     }
   }
+  .the-mandate {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    h1 {
+      text-align: center;
+      font-family: ;
+    }
+  }
 `;
 
 const StoreLink = styled(Link)`
   font-family: times new roman;
   text-decoration: none;
-  color: red;
+  color: black;
+  &:hover {
+    color: blue;
+  }
+`;
+
+const AllPrograms = styled(Link)`
+  margin: 10px 0 2.5cm 0;
+  background-color: rgb(240, 195, 60);
+  color: white;
+  text-decoration: none;
+  font-size: 1.6em;
+  font-family: times new roman;
+  padding: 0.5cm 0.8cm;
+  font-weight: bold;
+  border-radius: 20px;
+  &:hover {
+    background-color: rgb(220, 190, 60);
+  }
+`;
+
+const AboutContainer = styled(MainContainerContent)`
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  margin-top: 0;
+  padding-top: 0;
+  h1 {
+    background-color: rgba(252, 3, 3, 0.7);
+    color: white;
+    margin-top: 0;
+    text-align: center;
+    width: 100%;
+  }
 `;
